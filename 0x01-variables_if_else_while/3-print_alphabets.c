cb05d22970b3eeab1f15873ac86f991d4e3a8752
AMINA91608 <velmothcikada@gmail.com>
2023-06-19 15:54:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,17 @@
 int main(void)
 {
 	int n = 97;
-	int n = 65;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
+		n++;
 	}
 	while (n <= 90)
 	{
-		putchar(n);
+		putchar(m);
+		m++
 	}
 	putchar('\n');
 	return (0);
