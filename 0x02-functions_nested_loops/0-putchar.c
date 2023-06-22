073c95963d89ab6521168ecd886c54fb900f1972
AMINA91608 <velmothcikada@gmail.com>
2023-06-22 07:34:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
 /*
  * File: 0-holberton.c
  * Auth: Brennan D Baraban
- *
  */
 #include "holberton.h"
 /**
  * main - Prints "Holberton" followed by a new line.
- *
  * Return: Always 0.
  */
 int main(void)
