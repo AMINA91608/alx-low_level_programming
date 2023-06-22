91bd6a5cb81e634974777868c9a7ddc4191dfc81
AMINA91608 <velmothcikada@gmail.com>
2023-06-22 09:24:44 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - hiya wahed lfunctionse
+ * @n: hiyac wlahrt fojnction
+ * Return: 0 ila kannt 1 hh
+ */
+
+int print_last_digit(int n)
+{
+if (n >= 0)
+{
+_putchar('0' + (n % 10));
+return ((n % 10));
+}
+else
+{
+_putchar('0' + (-(n % 10)));
+return (-(n % 10));
+}
