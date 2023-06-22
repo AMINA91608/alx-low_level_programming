92461a3c1a19710995cae202015f2219d2dbc554
AMINA91608 <velmothcikada@gmail.com>
2023-06-22 09:10:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+int j = 0;
+
+while (j < 10)
+{
+j++;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
